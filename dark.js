f911ff8db3d742eb980f91beabf9fc7eafa13690
3shione-oss <3shione@gmail.com>
2026-01-10 15:20:48 +0900

    Initial commit

---
+++ dark.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("modeToggle");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    // 回転アニメーション
+    btn.style.transform = "rotate(360deg)";
+    setTimeout(() => {
+        btn.style.transform = "rotate(0deg)";
+    }, 600);
+
+    btn.textContent = document.body.classList.contains("dark") ? "☀" : "🌙";
+});
